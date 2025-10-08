a053fbe38b0c226a668c0114aed0f40f462c234e
aminefaw89 <mohamed.amine.ameur1@gmail.com>
2025-10-07 22:26:01 -0400

    Commentaire pour savoir corriger

--- controllers/music.controllers.js
+++ controllers/music.controllers.js
@@ -1,7 +1,24 @@
 import {Music} from "../models/music.model.js";
+// il faut importer aussi le model artiste pour faire les relations
+// import {Artiste} from "../models/artiste.model.js";
+
 export const createMusic = async (req, res) => {
     try {
+        // il faut que dans le body tu mets le titre et l'artisteId
+
+        // alors que dans le body tu mets juste le titre et tu as oublier l'artisteId
         const { titre} = req.body;
+        // const { artisteId } = req.body;
+
+        // il faut d'abord verifier si l'artiste existe
+
+        // const artiste = await Artiste.findByPk(artisteId);
+        // if (!artiste) {
+        //     return res.status(404).json({ error: 'Artiste not found' });
+        // }
+
+        // ensuite tu peux creer la musique en associant l'artiste
+        // const newMusic = await Music.create({ titre, artisteId});
         const newMusic = await Music.create({ titre});
         res.status(201).json(newMusic);
     }   catch (error) {
